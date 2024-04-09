a10f26a997f04371ce5971759bba1b4635f6ea33
Wilmer Guerrero <wilmermanuel.guerrero25@gmail.com>
2024-04-09 02:09:03 -0400

    Update vite.config.js to add resolve alias for vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
     plugins: [
@@ -15,5 +16,10 @@ export default defineConfig({
         rollupOptions: {
           input: 'resources/vue-client/src/main.js'
         }
-    }
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./resources/vue-client/src", import.meta.url)),
+        },
+    },
 });
